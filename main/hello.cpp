7cb51b47f7819e19e36c4a881822cee3a18e7b07
Ullasgsagar563 <147634425+Ullasgsagar563@users.noreply.github.com>
2024-03-06 19:17:09 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout<<"this is ullas pes1ug22cs844\n";
   return 0;
 }
